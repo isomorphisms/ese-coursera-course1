db49d5cb91d814f296b4c2dda27f3f9381430cd3
isomorphisms <isomorphisms@sdf.org>
2020-01-09 01:17:11 +0000

    this might work...

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -36,8 +36,85 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
 
 }
+
+/* Statistics and Printing Functions Go Here */
+
+void sort(unsigned int how_many, unsigned int *list_of_nums) {
+	int biggies[how_many/3]; // rounding — I’m OK ✓ with that
+	int littlies[how_many/3]; // rounding — I’m OK ✓ with that
+	int middlies[how_many/3]; // rounding — I’m OK ✓ with that
+		//actually the rounding may cause problems at the end. cracks. unless I fill in the cracks with 5th sandwich layers
+
+
+	//space concern?
+
+	//int bottom,one_third,two_thirds,top;
+	//I think I need two numbers for each cutpoint
+
+
+	//a three-layer cake with frosting and jelly above AND below each layer
+	volatile int minimum, top_lowie,   bottom_middlie, top_middlie,   bottom_biggie, maximum;
+	//a gift box with holes at the top AND bottom
+
+	// bread analogy?		table bottom (minimum), bread, lettuce, ....., lettuce, bread, air
+
+
+	//does this algorithm already exist?
+	// could it work with 5?
+	// does it have good time? (assuming space is maxed out here)
+	// how does that work with buffers?
+	// is comparison an assembly instruction?
+	// do the pushes and pops compile well into assembly?
+
+
+
+
+
+	//a sandwich buffer.
+	// this is not a problem because I’m setting up the cutpoints differently than going thorugh everything
+
+
+	// (do I need to send stuff to the "middle" when bottom/top pop overflows? )		//probably		//probably need to re-design the thing in this light
+
+	
+	volatile int first = * list_of_nums[0]
+	volatile int second = * list_of_nums[1]
+	volatile int third = * list_of_nums[2]
+	volatile int fourth = * list_of_nums[3]
+	volatile int fifth = * list_of_nums[4]
+
+
+	volatile int bigger, smaller;
+	if (first > second) { bigger = first; smaller = second; }
+	if (first < second) { smaller = first; bigger = second; }
+	
+
+	volatile int middle;
+	if (third > bigger) { middle = bigger; bigger = third; }
+	else if (third < smaller) { middle = smaller; smaller = third; }
+	else { middle = third; }
+
+
+
+
+
+	volatile int indice, anular;
+	//ring finger and index finger: fourth and second, referring to the cut-points (the jam) between the 3 layers of the cake
+
+
+	volatile int p, i, m, a, q;
+	// unified naming for the five layers
+
+
+
+
+
+	} //end sort
+
+
+
+
 
 /* Add other Implementation File Code Here */
